13a7d31162d0552318fefa4202baf73ed6dae43b
linjialiang <linjialiang@163.com>
2025-01-25 15:30:52 +0800

    update

--- assets/environment/cleanLog.sh
+++ assets/environment/cleanLog.sh
@@ -18,9 +18,9 @@ echo_cyan "是否清理zsh_history文件(1清理/默认不清理)："
 read num
 if [[ "$num" = "1" ]]; then
   echo_yellow "开始清理终端历史文件文件"
-  rm /home/{redis,mysqld-84,postgres,php-fpm,nginx,emad}/.{zsh,bash}_history
-  rm /home/{redis,mysqld-84,postgres,php-fpm,nginx,emad}/.{z,viminfo}
-  rm /home/{redis,mysqld-84,postgres,php-fpm,nginx,emad}/.zcompdump-*
+  rm /home/{sqlite,redis,mysqld-84,postgres,php-fpm,nginx,emad}/.{zsh,bash}_history
+  rm /home/{sqlite,redis,mysqld-84,postgres,php-fpm,nginx,emad}/.{z,viminfo}
+  rm /home/{sqlite,redis,mysqld-84,postgres,php-fpm,nginx,emad}/.zcompdump-*
   rm /root/.{zsh,bash}_history
   rm /root/.{z,viminfo}
   rm /root/.zcompdump-*
