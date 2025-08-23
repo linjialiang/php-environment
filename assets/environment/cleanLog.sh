64af063399f9935bd85b70419e234ab3683295e0
linjialiang <linjialiang@163.com>
2025-08-23 15:27:27 +0800

    update

--- assets/environment/cleanLog.sh
+++ assets/environment/cleanLog.sh
@@ -14,15 +14,24 @@ echo_yellow(){
   printf '\033[1;33m%b\033[0m\n' "$@"
 }
 
+echo_cyan "删除/var/cache/apt/archives/下所有包文件"
+apt clean
+echo_cyan "仅删除过期的缓存文件"
+apt autoclean
+echo_cyan "删除不再被依赖的软件包"
+apt autoremove --purge
+echo_cyan "立即清空日志"
+rm -rf /var/log/*
+
 echo_cyan "是否清理zsh_history文件(1清理/默认不清理)："
 read num
 if [[ "$num" = "1" ]]; then
   echo_yellow "开始清理终端历史文件文件"
   rm /home/{sqlite,redis,mysqld-84,postgres,php-fpm,nginx,emad}/.{zsh,bash}_history
-  rm /home/{sqlite,redis,mysqld-84,postgres,php-fpm,nginx,emad}/.{z,viminfo,bash_logout}
+  rm /home/{sqlite,redis,mysqld-84,postgres,php-fpm,nginx,emad}/.{z,viminfo}
   rm /home/{sqlite,redis,mysqld-84,postgres,php-fpm,nginx,emad}/.zcompdump-*
   rm /root/.{zsh,bash}_history
-  rm /root/.{z,viminfo,bash_logout}
+  rm /root/.{z,viminfo}
   rm /root/.zcompdump-*
   echo_yellow "清理终端历史文件结束"
 else
