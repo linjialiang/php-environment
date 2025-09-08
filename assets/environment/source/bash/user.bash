dd55f7f34687d8512d5f6659889fb74ba739f491
linjialiang <linjialiang@163.com>
2025-09-08 10:17:54 +0800

    update

--- assets/environment/source/bash/user.bash
+++ assets/environment/source/bash/user.bash
@@ -2,19 +2,22 @@
 
 # 创建 nginx 用户
 groupadd -g 2001 nginx
-useradd -c 'nginx service main process user' -g nginx -u 2001 -s /sbin/nologin -m nginx
+useradd -c 'nginx service main process user' \
+-g nginx -u 2001 -s /sbin/nologin -m nginx
 cp -r /root/{.oh-my-zsh,.zshrc} /home/nginx
 chown nginx:nginx -R /home/nginx/{.oh-my-zsh,.zshrc}
 
 # 创建 postgres 用户
 groupadd -g 2002 postgres
-useradd -c 'postgres service main process user' -g postgres -u 2002 -s /sbin/nologin -m postgres
+useradd -c 'postgres service main process user' \
+-g postgres -u 2002 -s /sbin/nologin -m postgres
 cp -r /root/{.oh-my-zsh,.zshrc} /home/postgres
 chown postgres:postgres -R /home/postgres/{.oh-my-zsh,.zshrc}
 
 # 创建 php-fpm 用户
 groupadd -g 2003 php-fpm
-useradd -c 'php-fpm service main process user' -g php-fpm -u 2003 -s /sbin/nologin -m php-fpm
+useradd -c 'php-fpm service main process user' \
+-g php-fpm -u 2003 -s /sbin/nologin -m php-fpm
 cp -r /root/{.oh-my-zsh,.zshrc} /home/php-fpm
 chown php-fpm:php-fpm -R /home/php-fpm/{.oh-my-zsh,.zshrc}
 
@@ -23,13 +26,15 @@ usermod -a -G postgres php-fpm
 
 # 创建 redis 用户
 groupadd -g 2005 redis
-useradd -c 'redis service main process user' -g redis -u 2005 -s /sbin/nologin -m redis
+useradd -c 'redis service main process user' \
+-g redis -u 2005 -s /sbin/nologin -m redis
 cp -r /root/{.oh-my-zsh,.zshrc} /home/redis
 chown redis:redis -R /home/redis/{.oh-my-zsh,.zshrc}
 
 # 新版本开始使用tcp转发，并不需要考虑socket文件转发相关的权限问题
 # php-fpm 主进程非特权用户时，需要考虑如下问题：
-# nginx 如果是通过 sock 文件代理转发给 php-fpm，php-fpm 主进程创建 sock 文件时需要确保 nginx 子进程用户有读写 sock 文件的权限
+# nginx 如果是通过 sock 文件代理转发给 php-fpm，
+#   php-fpm 主进程创建 sock 文件时需要确保 nginx 子进程用户有读写 sock 文件的权限
 # 方式1：采用 sock 文件权限 php-fpm:nginx 660 (nginx 权限较少，php-fpm 权限较多)
 # usermod -G nginx php-fpm
 # 方式2：采用 sock 文件权限 php-fpm:php-fpm 660 (nginx 权限较多，php-fpm 权限较少)
@@ -37,13 +42,15 @@ chown redis:redis -R /home/redis/{.oh-my-zsh,.zshrc}
 
 # 创建 MySQL 用户
 groupadd -g 2006 mysql
-useradd -c 'mysql service main process user' -g mysql -u 2006 -s /sbin/nologin -m mysql
+useradd -c 'mysql service main process user' \
+-g mysql -u 2006 -s /sbin/nologin -m mysql
 cp -r /root/{.oh-my-zsh,.zshrc} /home/mysql
 chown mysql:mysql -R /home/mysql/{.oh-my-zsh,.zshrc}
 
 # 创建 SQLite3 用户
 groupadd -g 2007 sqlite
-useradd -c 'sqlite main user' -g sqlite -u 2007 -s /sbin/nologin -m sqlite
+useradd -c 'sqlite main user' \
+-g sqlite -u 2007 -s /sbin/nologin -m sqlite
 cp -r /root/{.oh-my-zsh,.zshrc} /home/sqlite
 chown sqlite:sqlite -R /home/sqlite/{.oh-my-zsh,.zshrc}
 
