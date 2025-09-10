891b7df91ef14d04d8f055bfc8fe6e4091a4f093
linjialiang <linjialiang@163.com>
2025-09-10 10:49:22 +0800

    update

--- assets/environment/source/bash/user.bash
+++ assets/environment/source/bash/user.bash
@@ -41,7 +41,7 @@ useradd -c 'nginx service main process user' \
 cp -r /root/{.oh-my-zsh,.zshrc} /home/nginx
 chown nginx:nginx -R /home/nginx/{.oh-my-zsh,.zshrc}
 
-# 新版本开始使用tcp转发，并不需要考虑socket文件转发相关的权限问题
+# 新版本开始使用tcp/ip转发，并不需要考虑socket文件转发相关的权限问题
 # php-fpm 主进程非特权用户时，需要考虑如下问题：
 # nginx 如果是通过 sock 文件代理转发给 php-fpm，
 #   php-fpm 主进程创建 sock 文件时需要确保 nginx 子进程用户有读写 sock 文件的权限
@@ -49,10 +49,10 @@ chown nginx:nginx -R /home/nginx/{.oh-my-zsh,.zshrc}
 # usermod -G nginx php-fpm
 # 方式2：采用 sock 文件权限 php-fpm:php-fpm 660 (nginx 权限较多，php-fpm 权限较少)
 # usermod -G php-fpm nginx
+
 # php编译pgsql扩展，使用指定Postgres安装目录时，需提供读取libpg目录的权限
 # php使用到SQLite3的pkgconfig时，需提供读取SQLite3头和库文件的权限
-usermod -a -G postgres php-fpm
-usermod -a -G sqlite php-fpm
+usermod -a -G postgres,sqlite php-fpm
 
 # 部署环境注释，开发环境取消注释，开发用户追加附属组，其中emad指开发用户
 # - 部署环境不需要开发用户，可直接使用 nginx 用户作为 ftp、ssh 等上传工具的用户
