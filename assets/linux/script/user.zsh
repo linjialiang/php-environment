ab2a2fe8e73806160ce32ab20aa0a644e64757ea
linjialiang <linjialiang@163.com>
2026-01-23 14:46:48 +0800

    update

--- assets/linux/script/user.zsh
+++ assets/linux/script/user.zsh
@@ -5,7 +5,7 @@ users=(
   "2001:sqlite3"
   "2002:redis"
   "2003:postgres"
-  "2005:php-fpm"
+  "2005:php"
   "2006:nginx"
   "2007:mysql"
 );
@@ -29,21 +29,6 @@ for user in "${users[@]}"; do
     funcCreateUser "$user"
 done
 
-# 新版本开始使用tcp/ip转发，并不需要考虑socket文件转发相关的权限问题
-# php-fpm 主进程非特权用户时，需要考虑如下问题：
-# nginx 如果是通过 sock 文件代理转发给 php-fpm，
-#   php-fpm 主进程创建 sock 文件时需要确保 nginx 子进程用户有读写 sock 文件的权限
-# 方式1：采用 sock 文件权限 php-fpm:nginx 660 (nginx 权限较少，php-fpm 权限较多)
-# usermod -G nginx php-fpm
-# 方式2：采用 sock 文件权限 php-fpm:php-fpm 660 (nginx 权限较多，php-fpm 权限较少)
-# usermod -G php-fpm nginx
-
-# php编译pgsql扩展，使用指定Postgres安装目录时，需提供读取libpg目录的权限
-# php使用到SQLite3的pkgconfig时，需提供读取SQLite3头和库文件的权限
-usermod -a -G postgres,sqlite php-fpm
-
-# 部署环境注释，开发环境取消注释，开发用户追加附属组，其中emad指开发用户
-# - 部署环境不需要开发用户，可直接使用 nginx 用户作为 ftp、ssh 等上传工具的用户
-usermod -a -G emad nginx
-usermod -a -G emad php-fpm
-usermod -a -G sqlite,redis,postgres,php-fpm,nginx,mysql emad
+# 用户与组的关系
+usermod -a -G postgres,sqlite3 php
+usermod -a -G sqlite3,redis,postgres,php,nginx,mysql emad
