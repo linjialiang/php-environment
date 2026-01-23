98f0022b442e4531a4427e5cf6eca5f052561b3c
linjialiang <linjialiang@163.com>
2026-01-23 16:00:20 +0800

    update

--- assets/linux/script/chown.zsh
+++ assets/linux/script/chown.zsh
@@ -1,107 +1,72 @@
 #!/usr/bin/env bash
 
-FuncRunChown(){
+FuncRunPower(){
+    echo "----- 开始设置 $2 相关目录权限 -----"
     chown $2:$2 -R $1
-}
-
-FuncRunChmod(){
     chmod 750 -R $1
     find $1 -type f -exec chmod 640 {} \;
 }
 
-sqlite_array=(
+wwwItems=(
+    "/www"
+);
+
+sqlite3Items=(
     "/server/sqlite"
 );
 
-redis_array=(
+redisItems=(
     "/server/redis"
     "/server/redis/rdbData"
     "/server/logs/redis"
     "/server/etc/redis"
 );
 
-postgres_array=(
+postgresItems=(
     "/server/postgres"
     "/server/pgData"
     "/server/logs/postgres"
     "/server/etc/postgres"
 );
 
-mysql_array=(
-    "/server/mysql"
-    "/server/data"
-    "/server/logs/mysql"
-    "/server/etc/mysql"
-);
-
-phpFpm_array=(
+phpItems=(
     "/server/php"
     "/server/logs/php"
 );
 
-nginx_array=(
+nginxItems=(
     "/server/nginx"
     "/server/logs/nginx"
     "/server/etc/nginx"
     "/server/sites"
 );
 
-www_array=(
-    "/www"
+mysqlItems=(
+    "/server/mysql"
+    "/server/data"
+    "/server/logs/mysql"
+    "/server/etc/mysql"
 );
 
-echo "-----开始设置 sqlite 用户权限目录-----"
-for((i=0;i<${#chown_sqlite_array[*]};i++));
-do
-   echo ${chown_sqlite_array[i]}
-   func_chown_sqlite ${chown_sqlite_array[i]}
+# 循环
+for item in "${wwwItems[@]}"; do
+    FuncRunPower "$item" 'www'
 done
-echo "-----sqlite 用户权限目录设置结束-----"
-
-echo "-----开始设置 redis 用户权限目录-----"
-for((i=0;i<${#chown_redis_array[*]};i++));
-do
-   echo ${chown_redis_array[i]}
-   func_chown_redis ${chown_redis_array[i]}
+for item in "${sqlite3Items[@]}"; do
+    FuncRunPower "$item" 'sqlite3'
 done
-echo "-----redis 用户权限目录设置结束-----"
-
-echo "-----开始设置 postgres 用户权限目录-----"
-for((i=0;i<${#chown_postgres_array[*]};i++));
-do
-   echo ${chown_postgres_array[i]}
-   func_chown_postgres ${chown_postgres_array[i]}
+for item in "${redisItems[@]}"; do
+    FuncRunPower "$item" 'redis'
 done
-echo "-----postgres 用户权限目录设置结束-----"
-
-echo "-----开始设置 mysql 用户权限目录-----"
-for((i=0;i<${#chown_mysql_array[*]};i++));
-do
-   echo ${chown_mysql_array[i]}
-   func_chown_mysql ${chown_mysql_array[i]}
+for item in "${postgresItems[@]}"; do
+    FuncRunPower "$item" 'postgres'
 done
-echo "-----mysql 用户权限目录设置结束-----"
-
-echo "-----开始设置 php-fpm 用户权限目录-----"
-for((i=0;i<${#chown_phpFpm_array[*]};i++));
-do
-   echo ${chown_phpFpm_array[i]}
-   func_chown_phpFpm ${chown_phpFpm_array[i]}
+for item in "${phpItems[@]}"; do
+    FuncRunPower "$item" 'php'
 done
-echo "-----php-fpm 用户权限目录设置结束-----"
-
-echo "-----开始设置nginx用户权限目录-----"
-for((i=0;i<${#chown_nginx_array[*]};i++));
-do
-   echo ${chown_nginx_array[i]}
-   func_chown_nginx ${chown_nginx_array[i]}
+for item in "${nginxItems[@]}"; do
+    FuncRunPower "$item" 'nginx'
 done
-echo "-----nginx用户权限目录设置结束-----"
-
-echo "-----开始设置 开发者 用户权限目录-----"
-for((i=0;i<${#chown_www_array[*]};i++));
-do
-   echo ${chown_www_array[i]}
-   func_chown_www ${chown_www_array[i]}
+for item in "${mysqlItems[@]}"; do
+    FuncRunPower "$item" 'mysql'
 done
-echo "-----开发者 用户权限目录设置结束-----"
