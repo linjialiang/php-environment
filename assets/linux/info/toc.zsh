e04fc5f9f327c42de578a10dc864e45dbd8c24e8
linjialiang <linjialiang@163.com>
2026-01-23 16:21:24 +0800

    update

--- assets/linux/info/toc.zsh
+++ assets/linux/info/toc.zsh
@@ -1,41 +1,42 @@
-root ➜ ~ lt5 /server
-/server/
-├── data/
-├── default/
-├── etc/
-│   ├── mysql/
-│   ├── nginx/
-│   │   └── custom/
-│   ├── php/
-│   │   ├── 85/
-│   │   │   └── php-fpm.d/
-│   │   └── tools/
-│   ├── postgres/
-│   │   └── tls/
-│   └── redis/
-│       ├── config/
-│       │   └── custom/
-│       └── tls/
-├── logs/
-│   ├── mysql/
-│   │   └── binlog/
-│   ├── nginx/
-│   │   ├── access/
-│   │   └── error/
-│   ├── php/
-│   ├── postgres/
-│   │   └── wal_archive/
-│   └── redis/
-├── mysql/
-├── nginx/
-├── pgData/
-├── php/
-│   └── 85/
-├── postgres/
-├── redis/
-│   └── rdbData/
-├── sites/
-│   ├── available/
-│   ├── enabled/
-│   └── tls/
-└── sqlite3/
+root ➜ ~ lt5 -lg /server
+Permissions Size User     Group    Date Modified Name
+drwxr-xr-x     - root     root     23 1月  07:38 /server/
+drwxr-x---     - mysql    mysql    23 1月  07:38 ├── data/
+drwxr-xr-x     - root     root     23 1月  07:38 ├── default/
+drwxr-xr-x     - root     root     23 1月  07:38 ├── etc/
+drwxr-x---     - mysql    mysql    23 1月  07:38 │   ├── mysql/
+drwxr-x---     - nginx    nginx    23 1月  07:38 │   ├── nginx/
+drwxr-x---     - nginx    nginx    23 1月  07:38 │   │   └── custom/
+drwxr-x---     - php      php      23 1月  07:40 │   ├── php/
+drwxr-x---     - php      php      23 1月  07:38 │   │   ├── 85/
+drwxr-x---     - php      php      23 1月  07:38 │   │   │   └── php-fpm.d/
+drwxr-x---     - php      php      23 1月  07:38 │   │   └── tools/
+drwxr-x---     - postgres postgres 23 1月  07:38 │   ├── postgres/
+drwxr-x---     - postgres postgres 23 1月  07:38 │   │   └── tls/
+drwxr-x---     - redis    redis    23 1月  07:38 │   └── redis/
+drwxr-x---     - redis    redis    23 1月  07:38 │       ├── config/
+drwxr-x---     - redis    redis    23 1月  07:38 │       │   └── custom/
+drwxr-x---     - redis    redis    23 1月  07:38 │       └── tls/
+drwxr-xr-x     - root     root     23 1月  07:38 ├── logs/
+drwxr-x---     - mysql    mysql    23 1月  07:38 │   ├── mysql/
+drwxr-x---     - mysql    mysql    23 1月  07:38 │   │   └── binlog/
+drwxr-x---     - nginx    nginx    23 1月  07:38 │   ├── nginx/
+drwxr-x---     - nginx    nginx    23 1月  07:38 │   │   ├── access/
+drwxr-x---     - nginx    nginx    23 1月  07:38 │   │   └── error/
+drwxr-x---     - php      php      23 1月  07:38 │   ├── php/
+drwxr-x---     - postgres postgres 23 1月  07:38 │   ├── postgres/
+drwxr-x---     - postgres postgres 23 1月  07:38 │   │   └── wal_archive/
+drwxr-x---     - redis    redis    23 1月  07:38 │   └── redis/
+drwxr-x---     - mysql    mysql    23 1月  07:38 ├── mysql/
+drwxr-x---     - nginx    nginx    23 1月  07:38 ├── nginx/
+drwxr-x---     - postgres postgres 23 1月  07:38 ├── pgData/
+drwxr-x---     - php      php      23 1月  07:40 ├── php/
+drwxr-x---     - php      php      23 1月  07:38 │   └── 85/
+drwxr-x---     - postgres postgres 23 1月  07:38 ├── postgres/
+drwxr-x---     - redis    redis    23 1月  07:38 ├── redis/
+drwxr-x---     - redis    redis    23 1月  07:38 │   └── rdbData/
+drwxr-x---     - nginx    nginx    23 1月  07:38 ├── sites/
+drwxr-x---     - nginx    nginx    23 1月  07:38 │   ├── available/
+drwxr-x---     - nginx    nginx    23 1月  07:38 │   ├── enabled/
+drwxr-x---     - nginx    nginx    23 1月  07:38 │   └── tls/
+drwxr-x---     - sqlite3  sqlite3  23 1月  07:38 └── sqlite3/
