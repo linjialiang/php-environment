9729265489ce9afd457ce2099ea3ca6475e8b6c0
linjialiang <linjialiang@163.com>
2026-01-27 15:32:38 +0800

    update

--- assets/linux/script/redis/gen-test-certs.sh
+++ assets/linux/script/redis/gen-test-certs.sh
@@ -2,19 +2,19 @@
 
 # Generate some test certificates which are used by the regression test suite:
 #
-#   tls/ca.{crt,key}          Self signed CA certificate.
-#   tls/redis.{crt,key}       A certificate with no key usage/policy restrictions.
-#   tls/client.{crt,key}      A certificate restricted for SSL client usage.
-#   tls/server.{crt,key}      A certificate restricted for SSL server usage.
-#   tls/redis.dh              DH Params file.
+#   tests/tls/ca.{crt,key}          Self signed CA certificate.
+#   tests/tls/redis.{crt,key}       A certificate with no key usage/policy restrictions.
+#   tests/tls/client.{crt,key}      A certificate restricted for SSL client usage.
+#   tests/tls/server.{crt,key}      A certificate restricted for SSL server usage.
+#   tests/tls/redis.dh              DH Params file.
 
 generate_cert() {
     local name=$1
     local cn="$2"
     local opts="$3"
 
-    local keyfile=tls/${name}.key
-    local certfile=tls/${name}.crt
+    local keyfile=tests/tls/${name}.key
+    local certfile=tests/tls/${name}.crt
 
     [ -f $keyfile ] || openssl genrsa -out $keyfile 2048
     openssl req \
@@ -23,25 +23,25 @@ generate_cert() {
         -key $keyfile | \
         openssl x509 \
             -req -sha256 \
-            -CA tls/ca.crt \
-            -CAkey tls/ca.key \
-            -CAserial tls/ca.txt \
+            -CA tests/tls/ca.crt \
+            -CAkey tests/tls/ca.key \
+            -CAserial tests/tls/ca.txt \
             -CAcreateserial \
             -days 365 \
             $opts \
             -out $certfile
 }
 
-mkdir tls
-[ -f tls/ca.key ] || openssl genrsa -out tls/ca.key 4096
+mkdir -p tests/tls
+[ -f tests/tls/ca.key ] || openssl genrsa -out tests/tls/ca.key 4096
 openssl req \
     -x509 -new -nodes -sha256 \
-    -key tls/ca.key \
+    -key tests/tls/ca.key \
     -days 3650 \
     -subj '/O=Redis Test/CN=Certificate Authority' \
-    -out tls/ca.crt
+    -out tests/tls/ca.crt
 
-cat > tls/openssl.cnf <<_END_
+cat > tests/tls/openssl.cnf <<_END_
 [ server_cert ]
 keyUsage = digitalSignature, keyEncipherment
 nsCertType = server
@@ -51,8 +51,8 @@ keyUsage = digitalSignature, keyEncipherment
 nsCertType = client
 _END_
 
-generate_cert server "Server-only" "-extfile tls/openssl.cnf -extensions server_cert"
-generate_cert client "Client-only" "-extfile tls/openssl.cnf -extensions client_cert"
+generate_cert server "Server-only" "-extfile tests/tls/openssl.cnf -extensions server_cert"
+generate_cert client "Client-only" "-extfile tests/tls/openssl.cnf -extensions client_cert"
 generate_cert redis "Generic-cert"
 
-[ -f tls/redis.dh ] || openssl dhparam -out tls/redis.dh 2048
+[ -f tests/tls/redis.dh ] || openssl dhparam -out tests/tls/redis.dh 2048
