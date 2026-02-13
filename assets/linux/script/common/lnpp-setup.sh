f090bca05f5506c919b2fca218c9f2a039294a21
linjialiang <linjialiang@163.com>
2026-02-13 15:20:58 +0800

    update

--- assets/linux/script/common/lnpp-setup.sh
+++ assets/linux/script/common/lnpp-setup.sh
@@ -151,8 +151,8 @@ InstallBuild(){
 
 #重置Redis数字证书
 resetRedisCertificate(){
-  redisTlsPath=/server/redis/tls
-  redisTlsScriptPath=/server/redis/gen-test-certs.sh
+  redisTlsPath=/server/etc/redis/tls
+  redisTlsScriptPath=/server/etc/redis/gen-test-certs.sh
   rm -rf $redisTlsPath
   echo_yellow "=================================================================="
   echo_green "创建一键生成redis数字证书脚本"
@@ -284,7 +284,7 @@ _END_
 generate_cert server \"Server-only\" \"-extfile $pgsqlTlsPath/openssl.cnf -extensions server_cert\"
 generate_cert client \"Client-only\" \"-extfile $pgsqlTlsPath/openssl.cnf -extensions client_cert\"
 generate_cert client-admin \"admin\" \"-extfile $pgsqlTlsPath/openssl.cnf -extensions client_cert\"
-generate_cert client-emad \"emad\" \"-extfile $pgsqlTlsPath/openssl.cnf -extensions client_cert\"
+generate_cert client-replication \"repl_user\" \"-extfile $pgsqlTlsPath/openssl.cnf -extensions client_cert\"
 generate_cert pgsql \"Generic-cert\"
 
 [ -f $pgsqlTlsPath/pgsql.dh ] || openssl dhparam -out $pgsqlTlsPath/pgsql.dh 2048
